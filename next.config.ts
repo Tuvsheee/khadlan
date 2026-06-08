f57fb445e383ef26094ebcbf52c44a5f56b9af5b
tanuapp <developer.tanusoft@gmail.com>
2026-06-08 11:22:49 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "hadlan.tanusoft.mn",
+        hostname: "khadlan.tanusoft.mn",
         pathname: "/**",
       },
     ],
